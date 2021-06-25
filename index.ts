029ea4d1f341c093f4e62ac8dc26f2943b3ec44b
Spekksaver <fst081@gmail.com>
2021-06-25 13:00:09 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -4,3 +4,21 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+
+var metric = { 
+  "Name" : Text,
+  "Type" : [Text, Number, "Slider", "Other"],
+  "Range" : "Range or custom list",
+  "Series" : [{
+    "Date": Date,
+    "Source": Text,
+    "User": Text,
+    "value": "Various"
+  }],
+  "Icon" : "Ref",
+  "oAath" : Boolean,
+  "oAath-details" : {
+    "div1": Text
+  }
+};
